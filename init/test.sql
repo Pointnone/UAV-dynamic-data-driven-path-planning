c1404faf4c4516bfd71378ebc8424ba2d709e6b4
Point <christian@melsen.info>
2023-02-15 12:09:32 +0100

    SQL Init

---
+++ init/test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Test (
+    test1 Integer PRIMARY KEY NOT NULL,
+    test2 VARCHAR(25),
+    test3 Time
+)
--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export $(grep -v '^#' .env | xargs -d '\n')
 
-docker run --name postgres -e POSTGRES_PASSWORD=$DB_PASSWORD -p 127.0.0.1:5432:5432/tcp -d postgres:15.2-alpine
+docker run --name postgres -v `pwd`/init:/docker-entrypoint-initdb.d -e POSTGRES_PASSWORD=$DB_PASSWORD -p 127.0.0.1:5432:5432/tcp -d postgres:15.2-alpine
+
+pip3 install -r requirements.txt
+#python3 ./setup.py
