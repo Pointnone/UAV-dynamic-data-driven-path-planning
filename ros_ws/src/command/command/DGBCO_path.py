045982fc60bd2008af04dff588448297dacc136d
Pointnone <christian@melsen.info>
2023-04-12 13:12:13 +0200

    DGCO implemented

---
+++ ros_ws/src/command/command/DGBCO_path.py
@@ -0,0 +1,227 @@
+try:
+    from utils import *
+    from path_info_extract import *
+    from path_sim import *
+    from simple_path import _cost_path, Cost_Analyser
+except ImportError:
+    from .utils import *
+    from .path_info_extract import *
+    from .path_sim import *
+    from .simple_path import _cost_path, Cost_Analyser
+
+from shapely import Point, LineString, STRtree, distance, prepare, contains, intersects, envelope, buffer, box
+import pyproj as proj
+from datetime import datetime
+from functools import reduce
+import numpy as np
+
+import random as rnd
+
+tables = ["df", "cell", "zone"] # ["df", "dmi", "cell", "zone"]
+g = Geod(ellps="WGS84")
+
+class DGBCO_Particle:
+    def __init__(self, wps = [], num_ctr_points = 5, bounds = [0.0, 0.0, 0.0, 0.0]):
+        #self.wps = wps[1:-1]
+        self.home = wps[0]
+        self.dest = wps[-1]
+        self.bounds = bounds
+
+        self.wps = [ [rnd.uniform(bounds[0], bounds[2]), rnd.uniform(bounds[1], bounds[3])] for i in range(num_ctr_points) ]
+        #waypoints = g.inv_intermediate(self.home[0], self.home[1], self.dest[0], self.dest[1], num_ctr_points, initial_idx=0, terminus_idx=0)
+        #waypoints = [(lon, lat) for lon, lat in zip(waypoints.lons, waypoints.lats)]
+        #self.wps = waypoints
+
+    def cost(self, path_dat, start_time):
+        if(not hasattr(self, "cost_analyser")):
+            self.cost_analyser = Cost_Analyser(path_dat, [])
+
+        waypoints = [self.home]
+        #print(self.wps)
+        waypoints.extend(self.wps)
+        waypoints.extend([self.dest])
+        #waypoints = self.home + self.wps + self.dest
+        #print(waypoints)
+        self.total_cost, self.cost_per_waypoint = self.cost_analyser.analyse_cost(waypoints, start_time)
+        return self.total_cost
+    
+    def get_path(self):
+        waypoints = [self.home]
+        waypoints.extend(self.wps)
+        waypoints.extend([self.dest])
+        return waypoints
+    
+    def iterate(self, type, best, iter, max_iter):
+        vector = np.array(self.wps).flatten()
+        best_vector = np.array(best).flatten()
+        if(type == "e1"):
+            #print("Explore")
+            if(rnd.random() >= 0.5):
+                # Mutate
+                r = rnd.randint(0, len(vector)-1)
+                b_min = self.bounds[0] if r % 2 != 1 else self.bounds[1]
+                b_max = self.bounds[2] if r % 2 != 1 else self.bounds[3]
+                vector[r] = b_min + rnd.random() * (b_max - b_min)
+            else:
+                # Search around current solution
+                r1 = np.random.rand(len(vector)) * 2
+                r2 = np.random.rand(len(vector)) * 2
+
+                D = r1 * (vector - 1) # TODO: -1 ?
+                vector = vector + D * (r2 - 1)
+        else:
+            #print("Exploit")
+            if(rnd.random() >= 0.5):
+                # Move towards best
+                r3 = np.random.rand(len(vector)) * 2
+
+                D = r3 * (best_vector - vector)
+                vector = vector + D
+            else:
+                # Search around best
+                r4 = np.random.rand(len(vector))
+                r5 = np.random.rand(len(vector)) * 2
+
+                k = 2 - ((2*iter**2)/max_iter**2)
+                k_vect = [ k for i in range(len(vector)) ]
+
+                D = best_vector * (k_vect - r4)
+                vector = vector + D * (r5 - 1)
+
+        # Amend solution to within bounds
+        b_min_x = self.bounds[0]
+        b_min_y = self.bounds[1]
+        b_max_x = self.bounds[2]
+        b_max_y = self.bounds[3]
+                
+        for i,s in enumerate(vector):
+            if i % 2 == 0: # Even
+                if s < b_min_x:
+                    vector[i] = b_min_x
+                elif s > b_max_x:
+                    vector[i] = b_max_x
+            else: # Odd
+                if s < b_min_y:
+                    vector[i] = b_min_y
+                elif s > b_max_y:
+                    vector[i] = b_max_y
+        
+        waypoints = vector.reshape((-1, 2)).tolist()
+        #print(waypoints)
+        #waypoints = waypoints.tolist()
+        #waypoints.insert(0, self.wps[0])
+        #waypoints.insert(-1)
+        #print(waypoints[0])
+        self.wps = waypoints
+        return waypoints
+
+                # if(rnd.random() >= 0.5):
+                #     # Random waypoint
+                #     r = rnd.random(len(self.wps))
+                #     self.wps[r] = self.wps[r] + (rnd.random(), rnd.random()) * 0.
+                # else:
+                #     # Worst waypoint
+
+def _init_path(waypoints):
+    #waypoints = g.inv_intermediate(waypoints[0][0], waypoints[0][1], waypoints[1][0], waypoints[1][1], 5, initial_idx=1, terminus_idx=1)
+    #waypoints = [(lon, lat) for lon, lat in zip(waypoints.lons, waypoints.lats)]
+    return waypoints
+
+def _iterate_path(waypoints, path_dat, start_time, bounds, max_iter = 100):
+    # DGBCO
+    #print(bbox)
+    #bbox = bbox.coords
+    #bounds = [bbox[0], bbox[2]]
+    particles = [ DGBCO_Particle(waypoints, 5, bounds) for i in range(10) ]
+    #particles_exploitation = [ DGBCO_Particle(waypoints) for i in range(30) ]
+
+    #particles = particles_exploration + particles_exploitation
+
+    best_cost = float("inf")
+    best_path = []
+    prev1_cost = float("inf")
+    prev2_cost = float("inf")
+
+    perc_exploration = 0.7
+
+    for iter in range(max_iter):
+        print(f"Doing iteration {iter+1} of {max_iter}")
+        costs = np.array([ p.cost(path_dat, start_time) for p in particles ])
+        idx_min = costs.argmin()
+        min_cost = costs[idx_min]
+
+        # costs_e2 = np.array([ p.cost(path_dat, start_time) for p in particles_exploitation ])
+        # idx_min_e2 = costs_e2.argmin()
+        # e2_min_cost = costs_e2[idx_min_e2]
+
+        # idx_min = idx_min_e1 if e1_min_cost < e2_min_cost else idx_min_e2
+        # min_in_e1 = e1_min_cost < e2_min_cost
+
+        # TODO: Float comparison problem (Temp fix: Added + 1.0)
+        if(best_cost > min_cost):
+            print(f'Found new best: {min_cost}')
+            best_path = particles[idx_min].get_path()
+            best_cost = min_cost
+
+        k = 2 - (2*iter)/max_iter
+
+        # This is not well described in the algorithm pseudo-code or article. So I used some "common sense" instead. This might not be the best approach.
+        if(k < rnd.random() * 2): # Lean towards exploitation towards the end of search
+            #print("Adding more exploitation particles")
+            perc_exploration = perc_exploration - (perc_exploration) * rnd.random() * 0.12
+            print(f"perc_exploration is now: {perc_exploration}")
+
+        if(min_cost >= prev1_cost and costs[idx_min] >= prev2_cost):
+            #print("Adding more exploration particles")
+            perc_exploration = perc_exploration + (1 - perc_exploration) * rnd.random() * 0.06
+            print(f"perc_exploration is now: {perc_exploration}")
+            # for i in range(3):
+            #     if(len(particles_exploitation) >= 1):
+            #         particles_exploration.insert(0, particles_exploitation.pop(0)) # Pop one exploitation into exploration
+        prev2_cost = prev1_cost
+        prev1_cost = min_cost
+
+        for i,p in enumerate(particles):
+            if(i == idx_min): # Elitism
+                    continue
+            if(i <= len(particles) * perc_exploration):
+                # Exploration
+                #print("Exploration particle")
+                p.iterate("e1", particles[idx_min].wps, iter, max_iter)
+            else:
+                # Exploitation
+                #print("Exploitation particle")
+                p.iterate("e2", particles[idx_min].wps, iter, max_iter)
+        #print(particles)
+        rnd.shuffle(particles)
+        #print(particles)
+        
+    print(best_path)
+    print(best_cost)
+
+    print(to_wkt(LineString(best_path)))
+
+    return best_path, best_cost
+
+def find_path(home, dest, engine, meta, dbsm):
+    wps = [home, dest]
+
+    bounds = list(LineString(wps).bounds)
+    radius = 0.25
+    bounds[0] = bounds[0] - radius
+    bounds[1] = bounds[1] - radius
+    bounds[2] = bounds[2] + radius
+    bounds[3] = bounds[3] + radius
+    bbox = box(bounds[0], bounds[1], bounds[2], bounds[3]) 
+    path_dat = extract_db_data(tables, engine, meta, dbsm, wps, 0.25, bbox)
+
+    #path = [(10.3245895, 55.4718524), (10.3145895, 55.2518524), (10.2945895, 55.1518524)]
+    path = _init_path(wps)
+    path = _iterate_path(path, path_dat, datetime(2023, 4, 3, 12, 00, 00), bounds)
+
+    return path
+
+if(__name__ == "__main__"):
+    env = environmentVars()
+    engine, meta, dbsm = setupDB(env['DB_USER'], env['DB_PASS'])
+    find_path((10.3245895, 55.4718524), (10.3145895, 55.2518524), engine, meta, dbsm)
--- ros_ws/src/command/command/simple_path.py
+++ ros_ws/src/command/command/simple_path.py
@@ -19,6 +19,147 @@ DMI_TS_FORMAT = "%Y-%m-%dT%H%M%S"
 def _init_path(waypoints):
     return waypoints
 
+class Cost_Analyser():
+    def __init__(self, path_dat, zone_greenlist):
+        self.prepare_dat(path_dat, zone_greenlist)
+
+    def prepare_dat(self, path_dat, zone_greenlist):
+        if(hasattr(path_dat, "dmi")):
+            dmi_by_dts = {}
+            for p in path_dat['dmi']: # Linear pass to sort into buckets by timestamp
+                dt = datetime.strftime(p['dt'], DMI_TS_FORMAT)
+                if(dt in dmi_by_dts):
+                    dmi_by_dts[dt].extend([(Point(p['geom']), p)])
+                else:
+                    dmi_by_dts[dt] = [(Point(p['geom']), p)]
+
+            self.dmi = []
+            for dt in dmi_by_dts: # Build STRTree per timestamp for later lookup
+                stree = STRtree([ p[0] for p in dmi_by_dts[dt] ])
+                points = [ p[1] for p in dmi_by_dts[dt] ]
+                dmi_by_dts[dt] = {'stree': stree, 'points': points} # Keep point together for later cost calculation
+                self.dmi.append(dmi_by_dts[dt])
+            self.dmi_by_dts = dmi_by_dts
+            self.dmi_dts = [ datetime.strptime(dt, DMI_TS_FORMAT) for dt in dmi_by_dts ]
+
+        if(hasattr(path_dat, "cell")):
+            self.cell_dat = path_dat['cell']
+
+            # TODO: Filter by technology and radio available on drone
+            self.cell_stree = STRtree([ c['geom'] for c in self.cell_dat ])
+    
+        if(hasattr(path_dat, "df")):
+            cities = [ infs for infs in path_dat['df'] if infs['itype'] == "city" ]
+            #print(cities)
+
+            self.prepped_cities = [ (c['geom'], c) for c in cities ]
+            for pc in self.prepped_cities:
+                prepare(pc[0])
+        
+        if(hasattr(path_dat, "zone")):
+            notams = path_dat['notam']
+
+            self.notams_by_zname = {}
+            for n in notams:
+                self.notams_by_zname[n['zname']] = n
+            
+            zones = path_dat['zone']
+            notam_zones = [ n['zname'] for n in notams ]
+            activated_zones = [ z for z in zones if (z['zname'] in notam_zones and not z['zname'] in zone_greenlist) ]
+
+            # Prepare geometry for better performance
+            self.prepped_zones = [ (z['geom'], z) for z in activated_zones ]
+            for pz in self.prepped_zones:
+                prepare(pz[0])
+
+    def analyse_cost(self, waypoints, start_time):
+        epsg25832 = proj.CRS.from_epsg(25832)
+        epsg4326 = proj.CRS.from_epsg(4326)
+        transformer = proj.Transformer.from_crs(epsg4326, epsg25832, True)
+
+        sim_dist = 15.0
+        sim_temp_res = 1.0
+
+        pos, num_points = simpleSimulate(waypoints, start_time, sim_dist, sim_temp_res)
+        pos_nt = [ Point(p[0], p[1]) for p in pos ]
+
+        cost_per_pos = np.zeros(len(pos))
+
+        if(hasattr(self, "dmi")):
+            pos_map = []
+            for p in pos:
+                # Find shortest time
+                dt_delta = [ abs(p[2] - dt) for dt in self.dmi_dts ]
+                min_idx = np.argmin(dt_delta)
+                #min_dt = min(self.dmi_dts, key=lambda t: abs(p[2] - t))
+                dmi = self.dmi[min_idx]
+
+                # Find shortest point to point
+                nearest_idx = dmi['stree'].nearest(Point(p[0], p[1]))
+                pos_map.append(dmi['points'][nearest_idx]) # Snap positions to nearest position from DMI
+
+            precips = [ p['precip_meas'] for p in pos_map ]
+            windspds = [ p['windspd_meas'] for p in pos_map ]
+            windspds = [ (s if s >= 5.0 else 0.0) for s in windspds ]
+            cost_per_pos = cost_per_pos + precips + windspds
+
+        if(hasattr(self, "cell_stree")):
+            pos_map = []
+            for p in pos:
+                nearest_idx = self.cell_stree.nearest(Point(p[0], p[1]))
+                pos_map.append(self.cell_dat[nearest_idx])
+            
+            # Convert to EPSG:25832 to measure in meters
+            coord_convert = lambda lon,lat : Point(transformer.transform(lon,lat))
+            point_convert = lambda point : Point(transformer.transform(list(point['geom'].coords)[0][0], list(point['geom'].coords)[0][1]))
+            # Using distance even though it degenerates to Euclidean distance
+            dists_to_nearest = [ distance(coord_convert(p[0], p[1]), point_convert(cell)) for p, cell in zip(pos, pos_map) ]
+
+            # Calc cost based on min and max distances
+            # TODO: Consider basing this on technology type as well
+            min_dist = 100.0
+            max_dist = 3000.0
+            dist_cost_func = lambda d : 0.0 if d >= min_dist and d <= max_dist else (((d-max_dist)*0.003)**2 if d > max_dist else ((d-min_dist)*0.02)**10)
+            dist_cost = [ dist_cost_func(d) for d in dists_to_nearest ]
+            cost_per_pos = cost_per_pos + dist_cost
+
+        if(hasattr(self, "prepped_cities")):
+            for pc in self.prepped_cities:
+                pc_violations = contains(pc[0], pos_nt)
+                cost_per_violation = 10.0
+                city_cost = [ (cost_per_violation if pc_violations[i] else 0.0) for i, p in enumerate(pos) ]
+                cost_per_pos = cost_per_pos + city_cost
+        
+        if(hasattr(self, "prepped_zones")):
+            for pz in self.prepped_zones:
+                zname = pz[1]['zname']
+                pz_violations = contains(pz[0], pos_nt)
+
+                cost_per_violation = 100.0
+                zone_cost = [ (cost_per_violation if (pz_violations[i] and (p[2] >= self.notams_by_zname[zname]['act_from'] and p[2] <= self.notams_by_zname[zname]['act_to'])) else 0.0 ) for i, p in enumerate(pos) ]
+
+                cost_per_pos = cost_per_pos + zone_cost
+
+        cost_per_pos = cost_per_pos + ((sim_dist*sim_temp_res) / 10)
+        #print(len(cost_per_pos))
+
+        running = 1 # Start after first after home
+        cost_per_waypoint = np.zeros(len(waypoints))
+
+        for i,n in enumerate(num_points):
+            start = running
+            stop = running+n
+            segment = cost_per_pos[start:stop]
+            cost_per_waypoint[i+1] = reduce(lambda a,b: a+b, segment) if len(segment) > 0 else 0
+            running += n
+
+        total_cost = reduce(lambda a,b: a+b, cost_per_waypoint)
+        #print(cost_per_waypoint)
+        #print(total_cost)
+
+        return total_cost, cost_per_waypoint        
+
+
 def _cost_path(waypoints, path_dat, start_time, zone_greenlist = []):
     epsg25832 = proj.CRS.from_epsg(25832)
     epsg4326 = proj.CRS.from_epsg(4326)
@@ -48,7 +189,7 @@ def _cost_path(waypoints, path_dat, start_time, zone_greenlist = []):
 
         pos_map = []
         for p in pos:
-            # Find shortest time
+            # Find shortest time  
             min_dt = min(dmi_by_dts, key=lambda t: abs(p[2] - datetime.strptime(t, DMI_TS_FORMAT)))
             dmi = dmi_by_dts[min_dt]
 
@@ -127,7 +268,7 @@ def _cost_path(waypoints, path_dat, start_time, zone_greenlist = []):
             cost_per_pos = cost_per_pos + zone_cost
 
     cost_per_pos = cost_per_pos + ((sim_dist*sim_temp_res) / 10)
-    print(len(cost_per_pos))
+    #print(len(cost_per_pos))
 
     running = 1 # Start after first after home
     cost_per_waypoint = np.zeros(len(waypoints))
@@ -136,12 +277,12 @@ def _cost_path(waypoints, path_dat, start_time, zone_greenlist = []):
         start = running
         stop = running+n
         segment = cost_per_pos[start:stop]
-        cost_per_waypoint[i+1] = reduce(lambda a,b: a+b, segment)
+        cost_per_waypoint[i+1] = reduce(lambda a,b: a+b, segment) if len(segment) > 0 else 0
         running += n
 
     total_cost = reduce(lambda a,b: a+b, cost_per_waypoint)
-    print(cost_per_waypoint)
-    print(total_cost)
+    #print(cost_per_waypoint)
+    #print(total_cost)
 
     return total_cost, cost_per_waypoint
 
@@ -155,7 +296,8 @@ def find_path(home, dest, engine, meta, dbsm):
     wps = [home, dest]
     path_dat = extract_db_data(tables, engine, meta, dbsm, wps, 0.25)
 
-    path = [(10.3245895, 55.4718524), (10.3145895, 55.2518524), (10.2945895, 55.1518524)]#_init_path(wps)
+    #path = [(10.3245895, 55.4718524), (10.3145895, 55.2518524), (10.2945895, 55.1518524)]
+    path = _init_path(wps)
     path = _iterate_path(path, path_dat, datetime(2023, 4, 3, 12, 00, 00))
 
     return path
